8376e17b9737a6167e438a79e6b0a463ad80064a
harit <harit@LAPTOP-LEULO51L>
2022-01-19 12:46:38 -0600

    Program to read the numbers

---
+++ Latesttest/src/Read.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Read {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n;
+		
+		System.out.println("enter a number");
+		
+		Scanner s=new Scanner(System.in);
+		
+		n=s.nextInt();
+		
+		System.out.println("entered number is:"+n);
+		
+
+	}
+
+}
