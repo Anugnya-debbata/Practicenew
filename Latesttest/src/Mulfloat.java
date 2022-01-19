bc62feef5a91c6e58666f8bf9fe0c5c624d6143d
harit <harit@LAPTOP-LEULO51L>
2022-01-19 12:53:28 -0600

    float mul

---
+++ Latesttest/src/Mulfloat.java
@@ -0,0 +1,17 @@
+import java.io.*; 
+public class Mulfloat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float f1=3.5f;
+		float f2=5.7f;
+		
+		float p=f1*f2;
+		
+		System.out.println("the product is:"+p);
+		
+
+	}
+
+}
