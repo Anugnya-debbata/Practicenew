64c0484874a299b9fb3f556c3f65ec6f426c9f38
harit <harit@LAPTOP-LEULO51L>
2022-01-20 14:22:32 -0600

    1/20/22

---
+++ Latesttest/src/Sum.java
@@ -0,0 +1,16 @@
+
+public class Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=5;
+		int sum=0;
+		System.out.println("Sum of "+n+" are:");
+		for(int i=1;i<=n;i++) {
+			sum= i+sum;
+			
+		}
+		System.out.print(sum+" ");
+	}
+
+}
