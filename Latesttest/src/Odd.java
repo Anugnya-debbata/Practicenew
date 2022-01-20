de7eb5e2e16f8fd7a6492036bbeaf08eaeb09825
harit <harit@LAPTOP-LEULO51L>
2022-01-20 14:10:56 -0600

    1/20/22

---
+++ Latesttest/src/Odd.java
@@ -0,0 +1,17 @@
+
+public class Odd {
+
+	public static void main(String[] args) {
+		
+		int n=100;
+		
+		System.out.println("odd numbers between 1 to "+n+" are:");
+		for(int i=1;i<=n;i++) {
+			if(i%2==1) {
+				System.out.print(i+" ");			}
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
