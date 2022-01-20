0da6b659c1cb84569ed66a05d2b452a2dfcae737
harit <harit@LAPTOP-LEULO51L>
2022-01-20 14:07:10 -0600

    1/20/22

---
+++ Latesttest/src/Print.java
@@ -0,0 +1,18 @@
+
+public class Print {
+
+	public static void main(String[] args) {
+		
+		int n=100;
+		System.out.println("even numbers between 1 to "+n+" are:");
+		for(int i=2;i<=n;i++) {
+		if(i%2==0) {
+			
+			System.out.print(i+" ");
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+	}
+}
