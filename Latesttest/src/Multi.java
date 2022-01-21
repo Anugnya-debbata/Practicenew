e810bf9508ff7cc3c26bff6f45912222e4d6c0fa
harit <harit@LAPTOP-LEULO51L>
2022-01-21 15:33:51 -0600

    1/21/22

---
+++ Latesttest/src/Multi.java
@@ -0,0 +1,12 @@
+
+public class Multi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int n=13;
+for(int i=1;i<10;i++) {
+	System.out.println(n+" x "+i+" = "+n*i);
+}
+	}
+
+}
